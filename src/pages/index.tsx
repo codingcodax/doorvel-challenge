439306150a9ac07f7583991631eddc42a3c2a4f0
Alexis Guzman <codingcodax@gmail.com>
2023-02-14 14:06:09 -0600

    feat(home): get cat amenities parents array with get static props function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
+import type { GetStaticProps, NextPage } from 'next';
 import { Box, Typography } from '@mui/material';
-import { type NextPage } from 'next';
 
-const Home: NextPage = () => {
+import type { AmenityParent } from '~/types/amenities';
+
+type DataProps = {
+  data: AmenityParent[];
+  date_recived: Record<string, never>;
+};
+
+export const getStaticProps: GetStaticProps<{
+  amenitiesParents: AmenityParent[];
+}> = async () => {
+  const res = await fetch(
+    'http://54.177.198.128:8001/api/cat-amenities-parents/'
+  );
+
+  const data = (await res.json()) as DataProps;
+
+  return { props: { amenitiesParents: data.data } };
+};
+
+type HomeProps = {
+  amenitiesParents: AmenityParent[];
+};
+
+// eslint-disable-next-line react/prop-types
+const Home: NextPage<HomeProps> = ({ amenitiesParents }) => {
   return (
     <Box>
       <Typography>Home page</Typography>
