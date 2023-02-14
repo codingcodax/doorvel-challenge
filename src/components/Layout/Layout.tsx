91be92b32e5808d88cae052d61506ef66a90e40b
Alexis Guzman <codingcodax@gmail.com>
2023-02-14 11:56:03 -0600

    feat(components): create layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const Layout = () => {
+  return (
+    <Box>
+      <Typography>Layout</Typography>
+    </Box>
+  );
+};
+
+export default Layout;
