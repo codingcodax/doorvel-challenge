fb403151ded02bacdcb4c6a46cc2a2a99c9eade9
Alexis Guzman <codingcodax@gmail.com>
2023-02-14 11:45:24 -0600

    feat(app): add next styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,14 @@ import { CssBaseline } from '@mui/material';
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <style global jsx>{`
+        #__next {
+          min-height: 100vh;
+          display: grid;
+          grid-template-rows: auto 1fr auto;
+        }
+      `}</style>
       <CssBaseline />
       <Component {...pageProps} />
     </>
