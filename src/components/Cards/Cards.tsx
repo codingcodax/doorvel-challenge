7ee1ed376a4448a92de115e9781cbad5ad3a4781
Alexis Guzman <codingcodax@gmail.com>
2023-02-15 09:40:46 -0600

    feat(components): setup cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,10 +1,29 @@
-import { Box, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 
-const Cards = () => {
+import type { AmenityParent } from '~/types/amenities';
+
+import Card from './Card';
+
+type Props = {
+  items: AmenityParent[];
+};
+
+const Cards = ({ items }: Props) => {
   return (
-    <Box>
-      <Typography>Cards</Typography>
-    </Box>
+    <Grid container component='ul' mt={0} pl={0} spacing={2}>
+      {items.map((item) => (
+        <Grid
+          key={item.id}
+          item
+          component='li'
+          sm={6}
+          sx={{ listStyleType: 'none' }}
+          xs={12}
+        >
+          <Card name={item.name} side={item.created_by} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
