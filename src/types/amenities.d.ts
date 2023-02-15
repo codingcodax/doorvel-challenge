f4f0a6fa688671215e37ad12d73dc468021f387c
Alexis Guzman <codingcodax@gmail.com>
2023-02-15 14:08:13 -0600

    feat(types): create amenity parent and child

--- src/types/amenities.d.ts
+++ src/types/amenities.d.ts
@@ -1,5 +1,16 @@
 import type { z } from 'zod';
 
-import type { amenityParent } from '~/schemas/amenities';
+import type {
+  amenityParent,
+  amenityChild,
+  rawAmenityChild,
+} from '~/schemas/amenities';
 
 export type AmenityParent = z.infer<typeof amenityParent>;
+export type RawAmenityParent = {
+  data: AmenityParent[];
+  data_received: Record<string, never>;
+};
+
+export type AmenityChild = z.infer<typeof amenityChild>;
+export type RawAmenityChild = z.infer<typeof rawAmenityChild>;
