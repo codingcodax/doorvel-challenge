47099413348eba236ef3235051b4e74cacc7f150
Alexis Guzman <codingcodax@gmail.com>
2023-02-15 15:57:18 -0600

    feat(cards): add amenity type to cards props

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,14 +1,15 @@
 import { Grid } from '@mui/material';
 
-import type { AmenityParent } from '~/types/amenities';
+import type { AmenityChild, AmenityParent } from '~/types/amenities';
 
 import Card from './Card';
 
 type Props = {
-  items: AmenityParent[];
+  items: AmenityParent[] | AmenityChild[];
+  amenityType: 'parent' | 'child';
 };
 
-const Cards = ({ items }: Props) => {
+const Cards = ({ items, amenityType }: Props) => {
   return (
     <Grid container component='ul' mt={0} pl={0} spacing={2}>
       {items.map((item) => (
@@ -22,7 +23,7 @@ const Cards = ({ items }: Props) => {
           sx={{ listStyleType: 'none' }}
           xs={12}
         >
-          <Card name={item.name} side={item.created_by} />
+          <Card id={item.id} name={item.name} side={item.seo_friendly} />
         </Grid>
       ))}
     </Grid>
