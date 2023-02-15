9ef0a2e078700ee20070c23fb4e2de43d3373aad
Alexis Guzman <codingcodax@gmail.com>
2023-02-15 10:32:18 -0600

    feat(pages): create parent

---
+++ src/pages/parent/[name].tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Box, Typography } from '@mui/material';
+
+const Parent: NextPage = () => {
+  return (
+    <Box>
+      <Typography>Children</Typography>
+    </Box>
+  );
+};
+
+export default Parent;
