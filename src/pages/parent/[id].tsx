db19ac83474adb24933dacb6c6a4f0529e10fe37
Alexis Guzman <codingcodax@gmail.com>
2023-02-15 18:14:48 -0600

    feat(parent): add amenity parent object to get static props

--- src/pages/parent/[id].tsx
+++ src/pages/parent/[id].tsx
@@ -1,10 +1,11 @@
 import type { GetStaticProps, NextPage } from 'next';
 import { Box, Container, Typography } from '@mui/material';
 
-import type { AmenityChild } from '~/types/amenities';
+import type { AmenityChild, AmenityParent } from '~/types/amenities';
 import {
   getAmenitiesParents,
   getAmenitiesChildsByParentId,
+  getAmenityParent,
 } from '~/lib/amenity';
 import { ChildsCards } from '~/components';
 
@@ -17,27 +18,27 @@ export const getStaticPaths = async () => {
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps<{
+type Props = {
+  amenityParent: AmenityParent;
   amenitiesChilds: AmenityChild[];
-}> = async (context) => {
-  const id = context.params?.id as string;
-
-  const amenitiesChilds = await getAmenitiesChildsByParentId(+id);
-
-  return { props: { amenitiesChilds } };
 };
 
-type ParentProps = {
-  amenitiesChilds: AmenityChild[];
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const id = context.params?.id as string;
+
+  const amenityParent = await getAmenityParent(+id);
+  const amenitiesChilds = await getAmenitiesChildsByParentId(+id);
+
+  return { props: { amenityParent, amenitiesChilds } };
 };
 
 // eslint-disable-next-line react/prop-types
-const Parent: NextPage<ParentProps> = ({ amenitiesChilds }) => {
+const Parent: NextPage<Props> = ({ amenityParent, amenitiesChilds }: Props) => {
   return (
     <Container maxWidth='lg'>
       <Box>
         <Typography component='h2' textAlign='center' textTransform='uppercase'>
-          Parent:
+          Parent: {amenityParent.name}
         </Typography>
         <Typography>Select one:</Typography>
 
