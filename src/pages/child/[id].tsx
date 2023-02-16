0cd8e850a1765c809a05e0bfb31ee3f259f6f397
Alexis Guzman <codingcodax@gmail.com>
2023-02-15 18:15:01 -0600

    feat(child): add amenity parent to get server side props

--- src/pages/child/[id].tsx
+++ src/pages/child/[id].tsx
@@ -1,27 +1,33 @@
 import { Box, Container, Typography } from '@mui/material';
 import type { GetServerSideProps, NextPage } from 'next';
 
-import { getAmenityChild } from '~/lib/amenity';
-import type { AmenityChild } from '~/types/amenities';
+import { getAmenityChild, getAmenityParent } from '~/lib/amenity';
+import type { AmenityParent, AmenityChild } from '~/types/amenities';
 
-export const getServerSideProps: GetServerSideProps<{
+type Props = {
+  amenityParent: AmenityParent;
   amenityChild: AmenityChild;
-}> = async (context) => {
-  const id = context.params?.id as string;
-  const amenityChild = await getAmenityChild(+id);
-
-  return { props: { amenityChild } };
 };
 
-type ChildProps = {
-  amenityChild: AmenityChild;
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const id = context.params?.id as string;
+
+  const amenityParent = await getAmenityParent(+id);
+  const amenityChild = await getAmenityChild(+id);
+
+  return { props: { amenityParent, amenityChild } };
 };
 
 // eslint-disable-next-line react/prop-types
-const Child: NextPage<ChildProps> = ({ amenityChild }: ChildProps) => {
+const Child: NextPage<Props> = ({ amenityParent, amenityChild }: Props) => {
   return (
     <Container maxWidth='lg'>
       <Box>
+        <Typography component='h2' textAlign='center' textTransform='uppercase'>
+          Parent: ${amenityParent.name}
+        </Typography>
         <Typography>Name: {amenityChild.name}</Typography>
         <Typography>Parent ID: {amenityChild.id}</Typography>
         <Typography>Category ID: {amenityChild.property_category}</Typography>
