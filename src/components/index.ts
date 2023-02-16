d14e1cd7d1541061dae8d4ef37f0432e6c74c97c
Alexis Guzman <codingcodax@gmail.com>
2023-02-16 10:56:44 -0600

    feat(components): export go back

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as ParentsCards } from './Cards/ParentsCards';
 export { default as ChildsCards } from './Cards/ChildsCards';
 export { default as Footer } from './Footer';
+export { default as GoBack } from './GoBack';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
