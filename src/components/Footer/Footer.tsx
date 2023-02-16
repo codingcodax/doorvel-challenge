a4164d4673a5d24cb1afecad13659b4bb2f03a70
Alexis Guzman <codingcodax@gmail.com>
2023-02-15 18:03:48 -0600

    feat(footer): add link to codingcodax portfolio

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,19 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 
 const Footer = () => {
   return (
-    <Box>
-      <Typography>Footer</Typography>
+    <Box component='footer' pb={1}>
+      <Typography textAlign='center'>
+        Website built by{' '}
+        <Link
+          href='https://www.codingcodax.dev/'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          @codingcodax
+        </Link>{' '}
+        for Doorvel challenge
+      </Typography>
     </Box>
   );
 };
