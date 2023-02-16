b2c65706db9e7d896dfb83d47cc2c41ab578be8b
Alexis Guzman <codingcodax@gmail.com>
2023-02-16 10:51:05 -0600

    feat(components): create go back

---
+++ src/components/GoBack/GoBack.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const GoBack = () => {
+  return (
+    <Box>
+      <Typography>Go Back</Typography>
+    </Box>
+  );
+};
+
+export default GoBack;
