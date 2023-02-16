5c173f5b6aa008ea5f68cd17bda62905c2aad89f
Alexis Guzman <codingcodax@gmail.com>
2023-02-16 11:00:29 -0600

    feat(components): export title

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Footer } from './Footer';
 export { default as GoBack } from './GoBack';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
+export { default as Title } from './Title';
