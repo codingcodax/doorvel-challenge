ddd22b7e14a86cdf74a634e893c92e0ae51cbf65
Alexis Guzman <codingcodax@gmail.com>
2023-02-17 17:12:20 -0600

    feat(components): setup toggle theme

--- src/components/ToggleTheme/ToggleTheme.tsx
+++ src/components/ToggleTheme/ToggleTheme.tsx
@@ -1,10 +1,19 @@
-import { Box, Typography } from '@mui/material';
+import { DarkMode, LightMode } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+
+import { useTheme } from '~/hooks';
 
 const ToggleTheme = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <Box>
-      <Typography>Toggle Theme</Typography>
-    </Box>
+    <IconButton
+      aria-label='toggle theme'
+      sx={{ borderRadius: 0, color: 'text.primary' }}
+      onClick={toggleTheme}
+    >
+      {theme === 'light' ? <DarkMode /> : <LightMode />}
+    </IconButton>
   );
 };
 
