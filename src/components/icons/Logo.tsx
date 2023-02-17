95b86f0926be936ff9e719ce28a417d169bdc3c0
Alexis Guzman <codingcodax@gmail.com>
2023-02-17 10:51:29 -0600

    feat(icons): update logo colors

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -3,17 +3,17 @@ import { SvgIcon } from '@mui/material';
 const Logo = () => {
   return (
     <SvgIcon fontSize='large' viewBox='0 0 40 40'>
-      <circle cx='10' cy='10' fill='#000' r='5' />
+      <circle cx='10' cy='10' fill='currentColor' r='5' />
       <circle
         cx='30'
         cy='10'
         fill='none'
         r='4.5'
-        stroke='#000'
+        stroke='currentColor'
         strokeWidth={3}
       />
-      <circle cx='30' cy='30' fill='#000' r='5' />
-      <circle cx='10' cy='30' fill='#000' r='5' />
+      <circle cx='30' cy='30' fill='currentColor' r='5' />
+      <circle cx='10' cy='30' fill='currentColor' r='5' />
     </SvgIcon>
   );
 };
