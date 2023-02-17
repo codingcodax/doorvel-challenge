e23e61f3e7e50b3fa2ec8c0df92f1187cb300f22
Alexis Guzman <codingcodax@gmail.com>
2023-02-17 11:31:25 -0600

    feat(pages): create 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import { Box, Typography } from '@mui/material';
+
+const Custom404: NextPage = () => {
+  return (
+    <Box>
+      <Typography>404</Typography>
+    </Box>
+  );
+};
+
+export default Custom404;
