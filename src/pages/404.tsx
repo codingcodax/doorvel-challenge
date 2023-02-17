ec72dfe5fbff50ee07e0f39b42fc5a67e6811b86
Alexis Guzman <codingcodax@gmail.com>
2023-02-17 12:56:15 -0600

    feat(pages): setup 404 with responsive style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,44 @@
 import type { NextPage } from 'next';
-import { Box, Typography } from '@mui/material';
+import { Container, Button, Typography, Box } from '@mui/material';
+
+import { Link } from '~/components/ui';
+import { useMediaQuery } from '~/hooks';
 
 const Custom404: NextPage = () => {
+  const [isMobileSize, isTabletSize] = useMediaQuery();
+
   return (
-    <Box>
-      <Typography>404</Typography>
-    </Box>
+    <Container
+      maxWidth='lg'
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Box fontWeight='bold' textAlign='center'>
+        <Typography
+          fontSize={isTabletSize ? 150 : 250}
+          lineHeight='1'
+          variant='h1'
+        >
+          404
+        </Typography>
+        <Typography variant={isTabletSize ? 'h2' : 'h1'}>
+          We couldn&apos;t find this page.
+        </Typography>
+      </Box>
+      <Button
+        component={Link}
+        href='/'
+        size='large'
+        sx={{ mt: isMobileSize ? 4 : 8, borderRadius: 0, boxShadow: 'none' }}
+        variant='contained'
+      >
+        Go home
+      </Button>
+    </Container>
   );
 };
 
