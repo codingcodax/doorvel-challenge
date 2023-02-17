66e976125620de5e02017f474cb73e2434970513
Alexis Guzman <codingcodax@gmail.com>
2023-02-16 21:20:34 -0600

    feat(cards): add styles for dark mode

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,10 +1,10 @@
-import { Card, Typography, } from '@mui/material';
+import { Card, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 import type { AmenityChild, AmenityParent } from '~/types/amenities';
 import { Link } from '~/components/ui';
-
 import theme from '~/styles/theme';
+import { useDarkMode } from '~/hooks';
 
 type Props = {
   id: AmenityParent['id'] | AmenityChild['id'];
@@ -14,6 +14,7 @@ type Props = {
 };
 
 const MyCard = ({ id, side, name, amenityType }: Props) => {
+  const isDarkMode = useDarkMode();
   return (
     <Card
       noLinkStyle
@@ -27,8 +28,11 @@ const MyCard = ({ id, side, name, amenityType }: Props) => {
         boxShadow: 'none',
         border: `1px solid transparent`,
         borderRadius: 0,
-        backgroundColor: grey[100],
-        ':hover': { color: theme.palette.primary.main, borderColor: grey[300] },
+        backgroundColor: isDarkMode ? grey[900] : grey[100],
+        ':hover': {
+          color: theme.palette.primary.main,
+          borderColor: isDarkMode ? grey[700] : grey[300],
+        },
         transition: 'ease-in-out 200ms',
         transitionProperty: 'color, border',
         textDecoration: 'none',
