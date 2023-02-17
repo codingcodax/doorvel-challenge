ac1eb523c3bcec0af86bf0db28e5aec7dbab2482
Alexis Guzman <codingcodax@gmail.com>
2023-02-17 16:26:39 -0600

    feat(hooks): export use theme

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useDarkMode } from './use-dark-mode.hook';
 export { default as useHasMounted } from './use-has-mounted.hook';
 export { default as useMediaQuery } from './use-media-query.hook';
+export { default as useTheme } from './use-theme.hook';
