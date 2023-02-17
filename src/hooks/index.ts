6bd2cf5b67da23d1ae1c60a67f683d80a403ccd5
Alexis Guzman <codingcodax@gmail.com>
2023-02-17 10:33:22 -0600

    feat(hooks): export use has mounted

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useDarkMode } from './use-dark-mode.hook';
+export { default as useHasMounted } from './use-has-mounted.hook';
 export { default as useMobileSize } from './use-mobile-size.hook';
