e1b59a32ca33f96988456013d9b276ddf4d78e97
Alexis Guzman <codingcodax@gmail.com>
2023-02-17 16:33:40 -0600

    feat(components): create toggle theme

---
+++ src/components/ToggleTheme/ToggleTheme.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const ToggleTheme = () => {
+  return (
+    <Box>
+      <Typography>Toggle Theme</Typography>
+    </Box>
+  );
+};
+
+export default ToggleTheme;
