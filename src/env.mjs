e1f835057cd42890dde36b63612717f3a77cb687
Alexis Guzman <codingcodax@gmail.com>
2023-02-19 14:33:36 -0600

    feat(env): add libre translate base url

--- src/env.mjs
+++ src/env.mjs
@@ -16,6 +16,7 @@ const server = z.object({
  */
 const client = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+  NEXT_PUBLIC_LIBRE_TRANSLATE_BASE_URL: z.string().min(1),
 });
 
 /**
@@ -26,6 +27,8 @@ const client = z.object({
 const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_LIBRE_TRANSLATE_BASE_URL:
+    process.env.NEXT_PUBLIC_LIBRE_TRANSLATE_BASE_URL,
 };
 
 // Don't touch the part below
